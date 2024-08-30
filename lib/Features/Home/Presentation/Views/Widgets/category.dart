486bd3e18611eecd01f5f6cd29a1ed54e217a324
Mohamed Malky <elmalkymohamed278@gmail.com>
2024-08-30 17:14:52 +0300

    Some minor tweaks to the ClipPath section.

--- lib/Features/Home/Presentation/Views/Widgets/category.dart
+++ lib/Features/Home/Presentation/Views/Widgets/category.dart
@@ -1,5 +1,5 @@
 import 'package:shagaf_zag/Core/Barrel/imports.dart';
-
+import 'package:shagaf_zag/core/common/splasher.dart';
 
 class Category extends StatelessWidget {
   const Category({super.key});
@@ -10,110 +10,108 @@ class Category extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Padding(
-          padding: const EdgeInsets.only(left: 20,bottom: 10),
+          padding: const EdgeInsets.only(left: 20, bottom: 10),
           child: Text(
             "Category",
             style: ShagafFontStyles.darkGreyMedium20,
           ),
         ),
         SizedBox(
-          height: 350.h,
+          height: 380.h,
           child: Stack(
             children: [
               Positioned(
-                left: 20,
+                top: 0,
+                left: (MediaQuery.of(context).size.width - 342.w) / 2,
                 child: ClipPath(
-                  clipper: Potter(points: [
-                    const TopLeftCorner(),
-                    const TopRightCorner(),
-                    const PointOnRightSide(divider: 1.5),
-                    const BottomCenterCorner(),
-                    const PointOnLeftSide(divider: 1.5),
-                  ], radius: const Radius.circular(10)),
-                  child: InkWell(
-                    splashColor: Colors.transparent,
-                    highlightColor: Colors.transparent,
-                    onTap: (){},
-                    child: Container(
+                    clipper: Potter(points: [
+                      const TopLeftCorner(),
+                      const TopRightCorner(),
+                      const PointOnRightSide(divider: 1.5),
+                      const BottomCenterCorner(radius: Radius.circular(5)),
+                      const PointOnLeftSide(divider: 1.5),
+                    ], radius: const Radius.circular(10)),
+                    child: Splasher(
                       color: ShagafColors.primaryColor.withOpacity(0.65),
                       width: 342.w,
-                      height: 200.h,
+                      height: 220.h,
+                      onTap: () {},
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                           Padding(
-                             padding: const EdgeInsets.only(top: 60),
-                             child: Text("Room",style: ShagafFontStyles.whiteSemiBold24,),
-                           ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 60),
+                            child: Text(
+                              "Rooms",
+                              style: ShagafFontStyles.whiteSemiBold24,
+                            ),
+                          ),
                           SvgPicture.asset(room)
                         ],
                       ),
-                    ),
-                  ),
-                ),
+                    )),
               ),
               Positioned(
-                left: 20,
+                left: (MediaQuery.of(context).size.width - 342) / 2,
                 bottom: 0,
                 child: ClipPath(
-                  clipper: Potter(points: [
-                    const TopLeftCorner(radius: Radius.circular(30)),
-                    const PointOnRightSide(divider: 2.5),
-                    const BottomRightCorner(),
-                    const BottomLeftCorner(),
-                  ], radius: const Radius.circular(10)),
-                  child: InkWell(
-                    splashColor: Colors.transparent,
-                    highlightColor: Colors.transparent,
-                    child: Container(
-                      color: ShagafColors.secondaryColor.withOpacity(0.65),
-                      width: 163.w,
-                      height: 200.h,
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 50,left: 5),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                             Text("Orders",style: ShagafFontStyles.whiteSemiBold18,),
-                            SvgPicture.asset(order)
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
+                    clipper: Potter(points: [
+                      const TopLeftCorner(radius: Radius.circular(25)),
+                      const PointOnRightSide(divider: 2.5),
+                      const BottomRightCorner(),
+                      const BottomLeftCorner(),
+                    ], radius: const Radius.circular(10)),
+                    child: Splasher(
+                        onTap: () {},
+                        color: ShagafColors.secondaryColor.withOpacity(0.65),
+                        width: 163.w,
+                        height: 220.h,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 10),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Orders",
+                                style: ShagafFontStyles.whiteSemiBold18,
+                              ),
+                              SvgPicture.asset(order)
+                            ],
+                          ),
+                        ))),
               ),
               Positioned(
-                right: 20,
+                right: (MediaQuery.of(context).size.width - 342) / 2,
                 bottom: 0,
                 child: ClipPath(
                   clipper: Potter(points: [
                     const PointOnLeftSide(divider: 2.5),
-                    const TopRightCorner(radius: Radius.circular(30)),
+                    const TopRightCorner(radius: Radius.circular(25)),
                     const BottomRightCorner(),
                     const BottomLeftCorner(),
                   ], radius: const Radius.circular(10)),
-                  child: InkWell(
-                    splashColor: Colors.transparent,
-                    highlightColor: Colors.transparent,
-                    child: Container(
+                  child: Splasher(
+                      onTap: () {},
                       color: ShagafColors.tertiaryColor.withOpacity(0.65),
                       width: 163.w,
-                      height: 200.h,
+                      height: 220.h,
                       child: Padding(
-                        padding: const EdgeInsets.only(top: 50,right: 5),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                             Text("Events",style: ShagafFontStyles.whiteSemiBold18,),
+                          padding: const EdgeInsets.only(right: 10),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Text(
+                                "Events",
+                                style: ShagafFontStyles.whiteSemiBold18,
+                              ),
                               SvgPicture.asset(events)
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
+                            ],
+                          ))),
                 ),
               ),
             ],
