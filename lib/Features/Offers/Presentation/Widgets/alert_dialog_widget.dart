135e70350112b3a3510382b32adf810503b6fe58
YoussefTarek26 <y1000y100300@gmail.com>
2024-09-10 08:26:13 +0300

    Not completed

---
+++ lib/Features/Offers/Presentation/Widgets/alert_dialog_widget.dart
@@ -0,0 +1,60 @@
+import '../../../../Core/Barrel/imports.dart';
+
+class AlertDialogWidget extends StatelessWidget {
+  const AlertDialogWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      contentPadding: const EdgeInsets.all(18),
+      content: SizedBox(
+        width: 313.w,
+        height: 215.29.h,
+        child: SizedBox(
+              width: 262.14.w,
+              height: 167.29.h,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    width: 22.29.w,
+                    height: 17.14.h,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(discounts),
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "30% off  10 booking (up to EGP 150)",
+                    style: ShagafFontStyles.darkCyan14,
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: 149.w,
+                height: 33.h,
+                child: Image.asset(
+                  frame,
+                ),
+              ),
+              CustomButton(
+                  label: 'Copy',
+                  width: 113.w,
+                  height: 40.h,
+                  color: ShagafColors.secondaryColor,
+                  style: ShagafFontStyles.whiteNormal16,
+                  onTap: () {}
+              ),
+            ],
+          ),
+            ),
+        ),
+    );
+  }
+}
